072e7da580cc7a80e74b17663f79d53432bd1d61
Drew C <31282527+drewc5131@users.noreply.github.com>
2020-09-17 22:16:25 -0400

    cleanup more unused files

--- toontown/toon/DistributedNPCFlippyInToonHallAI.py
+++
@@ -1,6 +0,0 @@
-from .DistributedNPCToonAI import *
-
-class DistributedNPCFlippyInToonHallAI(DistributedNPCToonAI):
-
-    def __init__(self, air, npcId, questCallback=None, hq=0):
-        DistributedNPCToonAI.__init__(self, air, npcId, questCallback)
--- toontown/toonbase/ToonBaseGlobal.py
+++
@@ -1,5 +0,0 @@
-"""ToonBaseGlobal module: contains the global ToonBase instance"""
-
-from .ToonBase import *
-
-#__builtins__["base"] = ToonBase()
