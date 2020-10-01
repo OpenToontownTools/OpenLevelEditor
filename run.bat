b185f60d67d44228823484b87baa5292df95f8bf
Drew C <31282527+drewc5131@users.noreply.github.com>
2020-10-01 19:16:27 -0400

    oops. didnt mean to push

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 "C:/Panda3D-1.11.0-Py37-x64/python/python.exe" -m pip install -r requirements.txt
 :top
-"C:/Panda3D-1.11.0-Py37-x64/python/python.exe" -m ttle --experimental --hoods TT DD DG OZ phase_5/dna/toontown_central_2100.dna
+"C:/Panda3D-1.11.0-Py37-x64/python/python.exe" -m ttle --hoods TT
 pause
 goto top
