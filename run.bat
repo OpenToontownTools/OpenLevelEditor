b622f106b4d5c5636a594f627ac3bcdea48aa4b2
Drew C <31282527+drewc5131@users.noreply.github.com>
2020-11-04 16:53:40 -0500

    this isnt ready to push yet, oops

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-"C:/Panda3D-1.11.0-Py39-x64/python/python.exe" -m pip install -r requirements.txt
+"C:/Panda3D-1.11.0-Py37-x64/python/python.exe" -m pip install -r requirements.txt
 :top
-"C:/Panda3D-1.11.0-Py39-x64/python/python.exe" -m ttle --hoods BR
+"C:/Panda3D-1.11.0-Py37-x64/python/python.exe" -m ttle --hoods TT
 pause
 goto top
