d2401916be50b9401d54fde2f83c8a7d0d876302
Drew C <31282527+drewc5131@users.noreply.github.com>
2020-11-15 17:12:25 -0500

    font

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 "C:/Panda3D-1.11.0-Py39-x64/python/python.exe" -m pip install -r requirements.txt
 :top
-"C:/Panda3D-1.11.0-Py39-x64/python/python.exe" -m ttle --hoods TT
+"C:/Panda3D-1.11.0-Py39-x64/python/python.exe" -m ttle --hoods BR
 pause
 goto top
