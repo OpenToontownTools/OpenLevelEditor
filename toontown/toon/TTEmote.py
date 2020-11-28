f52e818cffbbc3d5bfd046f5a4b010bff4c1e6ca
Nathan S <nathanstgeorge7@gmail.com>
2020-11-27 21:03:56 -0500

    base.loadSfx->loader.loadSfx

--- toontown/toon/TTEmote.py
+++ toontown/toon/TTEmote.py
@@ -43,7 +43,7 @@ EmoteClear = -1
 
 def doVictory(toon, volume = 1):
     duration = toon.getDuration('victory', 'legs')
-    sfx = base.loadSfx("phase_3.5/audio/sfx/ENC_Win.mp3")
+    sfx = loader.loadSfx("phase_3.5/audio/sfx/ENC_Win.mp3")
 
     # Loop this sound effect for the appropriate duration
     sfxDuration = duration - 1.0
@@ -71,9 +71,9 @@ def doAnnoyed(toon, volume = 1):
     # This sfx is in phase 3.5 because it can be triggered during the tutorial
     sfx = None
     if (toon.style.getAnimal() == 'bear'):
-        sfx = base.loadSfx("phase_3.5/audio/dial/AV_bear_exclaim.mp3")
+        sfx = loader.loadSfx("phase_3.5/audio/dial/AV_bear_exclaim.mp3")
     else:
-        sfx = base.loadSfx("phase_3.5/audio/sfx/avatar_emotion_angry.mp3")
+        sfx = loader.loadSfx("phase_3.5/audio/sfx/avatar_emotion_angry.mp3")
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx():
         base.playSfx(sfx, volume = volume, node = toon)
@@ -171,7 +171,7 @@ def doOk(toon, volume = 1):
 
 def doShrug(toon, volume = 1):
     # This sfx is in phase 3.5 because it can be triggered during the tutorial
-    sfx = base.loadSfx("phase_3.5/audio/sfx/avatar_emotion_shrug.mp3")
+    sfx = loader.loadSfx("phase_3.5/audio/sfx/avatar_emotion_shrug.mp3")
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx():
         base.playSfx(sfx, volume = volume, node = toon)
@@ -186,7 +186,7 @@ def doWave(toon, volume = 1):
     return track, duration, None
 
 def doApplause(toon, volume = 1):
-    sfx = base.loadSfx("phase_4/audio/sfx/avatar_emotion_applause.mp3")
+    sfx = loader.loadSfx("phase_4/audio/sfx/avatar_emotion_applause.mp3")
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx():
         base.playSfx(sfx, volume = 1, node = toon)
@@ -196,7 +196,7 @@ def doApplause(toon, volume = 1):
     return track, duration, None
 
 def doConfused(toon, volume =1):
-    sfx = base.loadSfx("phase_4/audio/sfx/avatar_emotion_confused.mp3")
+    sfx = loader.loadSfx("phase_4/audio/sfx/avatar_emotion_confused.mp3")
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx():
         base.playSfx(sfx, node = toon, volume = volume)
@@ -206,7 +206,7 @@ def doConfused(toon, volume =1):
     return track, duration, None
 
 def doSlipForward(toon, volume = 1):
-    sfx = base.loadSfx("phase_4/audio/sfx/MG_cannon_hit_dirt.mp3")
+    sfx = loader.loadSfx("phase_4/audio/sfx/MG_cannon_hit_dirt.mp3")
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx():
         base.playSfx(sfx, volume = volume, node = toon)
@@ -218,7 +218,7 @@ def doSlipForward(toon, volume = 1):
     return track, duration, None
 
 def doBored(toon, volume = 1):
-    sfx = base.loadSfx("phase_4/audio/sfx/avatar_emotion_bored.mp3")
+    sfx = loader.loadSfx("phase_4/audio/sfx/avatar_emotion_bored.mp3")
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx():
         base.playSfx(sfx, volume = volume, node = toon)
@@ -241,7 +241,7 @@ def doBow(toon, volume = 1):
     return track, duration, None
 
 def doSlipBackward(toon, volume = 1):
-    sfx = base.loadSfx("phase_4/audio/sfx/MG_cannon_hit_dirt.mp3")
+    sfx = loader.loadSfx("phase_4/audio/sfx/MG_cannon_hit_dirt.mp3")
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx():
         base.playSfx(sfx, volume = volume, node = toon)
@@ -285,7 +285,7 @@ def doNothing(toon, volume =1):
 
 def doSurprise(toon, volume = 1):
     sfx = None
-    sfx = base.loadSfx("phase_4/audio/sfx/avatar_emotion_surprise.mp3")
+    sfx = loader.loadSfx("phase_4/audio/sfx/avatar_emotion_surprise.mp3")
 
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx(volume = 1):
@@ -317,7 +317,7 @@ def doSurprise(toon, volume = 1):
 
 def doUpset(toon, volume = 1):
     sfx = None
-    sfx = base.loadSfx("phase_4/audio/sfx/avatar_emotion_very_sad_1.mp3")
+    sfx = loader.loadSfx("phase_4/audio/sfx/avatar_emotion_very_sad_1.mp3")
 
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx(volume = 1):
@@ -345,7 +345,7 @@ def doUpset(toon, volume = 1):
 
 def doDelighted(toon, volume = 1):
     sfx = None
-    sfx = base.loadSfx('phase_4/audio/sfx/delighted_06.mp3')
+    sfx = loader.loadSfx('phase_4/audio/sfx/delighted_06.mp3')
 
     def playSfx(volume = 1):
         base.playSfx(sfx, volume = volume, node = toon)
@@ -376,7 +376,7 @@ def doFurious(toon, volume = 1):
     duration = toon.getDuration('angry', 'torso')
     # This sfx is in phase 3.5 because it can be triggered during the tutorial
     sfx = None
-    sfx = base.loadSfx("phase_4/audio/sfx/furious_03.mp3")
+    sfx = loader.loadSfx("phase_4/audio/sfx/furious_03.mp3")
 
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx(volume = 1):
@@ -395,7 +395,7 @@ def doFurious(toon, volume = 1):
 
 def doLaugh(toon, volume = 1):
     sfx = None
-    sfx = base.loadSfx("phase_4/audio/sfx/avatar_emotion_laugh.mp3")
+    sfx = loader.loadSfx("phase_4/audio/sfx/avatar_emotion_laugh.mp3")
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx(volume = 1):
         base.playSfx(sfx, volume = volume, node = toon)
@@ -423,7 +423,7 @@ def getSingingNote(toon, note, volume = 1):
     filePrefix = "tt_s_dlg_sng_"
     fileSuffix = ".mp3"
     speciesName = ToonDNA.getSpeciesName(toon.style.head)
-    sfx = base.loadSfx(filePath + filePrefix + speciesName + "_" + note + fileSuffix)
+    sfx = loader.loadSfx(filePath + filePrefix + speciesName + "_" + note + fileSuffix)
     # Need to use a Func interval since DoEmote expects a 0 duration track
     def playSfx(volume = 1):
         base.playSfx(sfx, volume = volume, node = toon)
