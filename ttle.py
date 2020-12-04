71924789a4dabf6f1ffc274b9ccb618a1a862102
Drew C <31282527+drewc5131@users.noreply.github.com>
2020-12-04 15:40:38 -0500

    code cleanup

--- ttle.py
+++ ttle.py
@@ -20,10 +20,11 @@ TOONTOWN_REWRITTEN = 1
 TOONTOWN_CORPORATE_CLASH = 2
 TOONTOWN_OFFLINE = 3
 
-SERVER_TO_ID = {'online': TOONTOWN_ONLINE,
+SERVER_TO_ID = {'online':    TOONTOWN_ONLINE,
                 'rewritten': TOONTOWN_REWRITTEN,
-                'clash': TOONTOWN_CORPORATE_CLASH,
-                'offline': TOONTOWN_OFFLINE}
+                'clash':     TOONTOWN_CORPORATE_CLASH,
+                'offline':   TOONTOWN_OFFLINE
+                }
 
 DEFAULT_SERVER = TOONTOWN_ONLINE
 
@@ -44,23 +45,25 @@ class ToontownLevelEditor(ShowBase):
             pathlib.Path(userfiles).mkdir(parents = True, exist_ok = True)
 
         # Check for -e or -d launch options
-        parser = argparse.ArgumentParser(description="Modes")
-        parser.add_argument("--experimental", action='store_true', help="Enables experimental features")
-        parser.add_argument("--debug", action='store_true', help="Enables debugging features")
-        parser.add_argument("--noupdate", action='store_true', help="Disables Auto Updating")
-        parser.add_argument("--compiler", nargs = "*", help="Specify which compiler to use (Only useful if your game uses a form of "
-                                                            "libpandadna.) Valid options are 'libpandadna', for games which use the "
-                                                            "modern c++ version of libpandadna (like Toontown Offline), and 'clash', "
-                                                            "for games that use the legacy python version of libpandadna, mainly Corporate Clash")
+        parser = argparse.ArgumentParser(description = "Modes")
+        parser.add_argument("--experimental", action = 'store_true', help = "Enables experimental features")
+        parser.add_argument("--debug", action = 'store_true', help = "Enables debugging features")
+        parser.add_argument("--noupdate", action = 'store_true', help = "Disables Auto Updating")
+        parser.add_argument("--compiler", nargs = "*",
+                            help = "Specify which compiler to use (Only useful if your game uses a form of "
+                                   "libpandadna.) Valid options are 'libpandadna', for games which use the "
+                                   "modern c++ version of libpandadna (like Toontown Offline), and 'clash', "
+                                   "for Corporate Clash")
 
-        parser.add_argument("--server", nargs="*", help="Enables features exclusive to various Toontown projects", default='online')
-        parser.add_argument("--holiday", nargs="*", help="Enables holiday modes. [halloween or winter]")
-        parser.add_argument("--hoods", nargs="*", help="Only loads the storage files of the specified hoods",
-                            default=['TT', 'DD', 'BR', 'DG',
-                                     'DL', 'MM', 'GS', 'GZ',
-                                     'SBHQ', 'LBHQ', 'CBHQ', 'BBHQ',
-                                     'OZ', 'PA', 'ES', 'TUT'])
-        parser.add_argument("dnaPath", nargs="?", help="Load the DNA file through the specified path")
+        parser.add_argument("--server", nargs = "*", help = "Enables features exclusive to various Toontown projects",
+                            default = 'online')
+        parser.add_argument("--holiday", nargs = "*", help = "Enables holiday modes. [halloween or winter]")
+        parser.add_argument("--hoods", nargs = "*", help = "Only loads the storage files of the specified hoods",
+                            default = ['TT', 'DD', 'BR', 'DG',
+                                       'DL', 'MM', 'GS', 'GZ',
+                                       'SBHQ', 'LBHQ', 'CBHQ', 'BBHQ',
+                                       'OZ', 'PA', 'ES', 'TUT'])
+        parser.add_argument("dnaPath", nargs = "?", help = "Load the DNA file through the specified path")
 
         args = parser.parse_args()
         if args.experimental:
@@ -117,15 +120,16 @@ class ToontownLevelEditor(ShowBase):
     def toggleFrameRateMeter(self, flag):
         if flag:
             if not self.frameRateMeter:
-                self.frameRateMeter = OnscreenText(parent = base.a2dTopRight, text = '', pos = (-0.1125, -0.05, 0.0), scale = 0.05, style = 3,
-                                                   bg = (0, 0, 0, 0.4), font = ToontownGlobals.getToonFont())
-                taskMgr.add(self.frameRateCounter, 'fps')
+                self.frameRateMeter = OnscreenText(parent = base.a2dTopRight, text = '', pos = (-0.1125, -0.05, 0.0),
+                                                   scale = 0.05, style = 3, bg = (0, 0, 0, 0.4),
+                                                   font = ToontownGlobals.getToonFont())
+                taskMgr.add(self.updateFrameRateMeter, 'fps')
         else:
             if self.frameRateMeter:
                 self.frameRateMeter.destroy()
                 self.frameRateMeter = None
 
-    def frameRateCounter(self, task):
+    def updateFrameRateMeter(self, task):
         """
         Base code inspired from
         https://discourse.panda3d.org/t/trying-to-create-custom-fps-counter/25328/15
@@ -161,9 +165,9 @@ class ToontownLevelEditor(ShowBase):
 
     def __importMainLibs(self):
         builtin_dict = builtins.__dict__
-        builtin_dict.update(__import__('panda3d.core', fromlist=['*']).__dict__)
-        builtin_dict.update(__import__('libotp', fromlist=['*']).__dict__)
-        builtin_dict.update(__import__('libtoontown', fromlist=['*']).__dict__)
+        builtin_dict.update(__import__('panda3d.core', fromlist = ['*']).__dict__)
+        builtin_dict.update(__import__('libotp', fromlist = ['*']).__dict__)
+        builtin_dict.update(__import__('libtoontown', fromlist = ['*']).__dict__)
 
     def __createTk(self):
         tkroot = Tk()
@@ -184,17 +188,23 @@ class ToontownLevelEditor(ShowBase):
         import aiohttp, webbrowser
         async with aiohttp.ClientSession() as session:
             try:
-                async with session.get("https://raw.githubusercontent.com/OpenToontownTools/OpenLevelEditor/master/ver") as resp:
+                async with session.get(
+                        "https://raw.githubusercontent.com/OpenToontownTools/OpenLevelEditor/master/ver") as resp:
                     ver = await resp.text()
                     ver = ver.splitlines()[0]
                     if ver != self.APP_VERSION:
                         self.notify.info(f"Client is out of date! Latest: {ver} | Client: {self.APP_VERSION}")
-                        if messagebox.askokcancel("Error", f"Client is out of date!\nLatest: {ver} | Client: {self.APP_VERSION}. Press OK to be taken to the download page."):
+                        if messagebox.askokcancel("Error",
+                                                  f"Client is out of date!\nLatest: {ver} | Client: {self.APP_VERSION}. "
+                                                  f"Press OK to be taken to the download page."):
                             webbrowser.open("https://github.com/OpenToontownTools/OpenLevelEditor/releases/latest")
                     else:
                         self.notify.info("Client is up to date!")
             except:
-                messagebox.showerror(message = "There was an error checking for updates! This is likely an issue with your connection. Press OK to continue using the application.")
+                messagebox.showerror(
+                    message = "There was an error checking for updates! This is likely an issue with your connection. "
+                              "Press OK to continue using the application.")
+
 
 # Run it
 ToontownLevelEditor().run()
